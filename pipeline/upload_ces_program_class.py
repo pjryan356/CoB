11f3805e88aa1206a535a2cb83ac24e634ee34e2
pjryan356 <peter.ryan2@rmit.edu.au>
2018-11-22 13:33:10 +1100

    Update pipeline/upload_ces_program_class.py
    This file was a cop of a different file. I t now does what it is suppose to

--- pipeline/upload_ces_program_class.py
+++ pipeline/upload_ces_program_class.py
@@ -23,54 +23,59 @@ postgres_engine = create_engine(engine_string)
 postgres_con = postgres_engine.connect()
 
 
-def upload_course_data_from_excel(directory, filename, engine,
-                                  course_tbl_name='tbl_course_summaries',
-                                  class_teacher_tbl_name='tbl_class_teacher_summaries',
+def upload_program_course_data_from_excel(directory, filename, engine,
+                                  program_course_tbl_name='tbl_program_course_post2018',
+                                  program_tbl_name='tbl_program_post2018',
                                   schema='ces'):
   # gets course ces data split by program from the suuplied excel files and uploads them to postrgres
-  df = pd.read_excel(directory + filename,
-                     sheet_name='Sheet1',
-                     skiprows=4,
-                     usecols=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27],
-                     skipfooter=13)
-
-  df.columns = ['course_code_ces','all_flag', 'class_nbr', 'term_code', 'section_code', 'course_name',
-                'teaching_staff', 'course_coordinator', 'career',
-                'population', 'osi_count', 'gts_count', 'reliability', 'campus',
-                'gts', 'gts_mean', 'osi', 'osi_mean',
-                'international_count', 'domestic_count', 'ft_count', 'pt_count',
-                'gts1', 'gts2', 'gts3', 'gts4', 'gts5', 'gts6']
-
-  f_split=filename.split('.')[0].split()
-
+  f_split = filename.split('.')[0].split()
   level = f_split[3][1:3] # brackets removed
-  semester = f_split[6][:-1] # comma removed
-  year = f_split[7][:-1] # bracket removed
+  semester = int(f_split[6][:-1]) # comma removed
+  year = int(f_split[7][:-1]) # bracket removed
+  program_code = f_split[4].split('-')[1] # remove school_code
+  
+  
+  if year < 2018:
+    df = pd.read_excel(directory + filename,
+                       sheet_name='Sheet1',
+                       skiprows=4,
+                       usecols=[0, 2, 4, 8, 9, 10, 11, 13, 14, 15, 16, 17, 18, 19, 20, 21],
+                       skipfooter=13)
+  
+    df.columns = ['course_code_ces', 'class_nbr',
+                  'all_flag',
+                  'population', 'osi_count', 'gts_count', 'reliability',
+                  'gts', 'gts_mean', 'osi',
+                  'gts1', 'gts2', 'gts3', 'gts4', 'gts5', 'gts6']
+    df['osi_mean'] = None
+    
+  else:
+    df = pd.read_excel(directory + filename,
+                       sheet_name='Sheet1',
+                       skiprows=4,
+                       usecols=[0, 2, 4, 8,9,10,11, 13,14,15,16, 17,18,19,20,21,22],
+                       skipfooter=13)
+  
+    df.columns = ['course_code_ces', 'class_nbr',
+                  'all_flag',
+                  'population', 'osi_count', 'gts_count', 'reliability',
+                  'gts', 'gts_mean', 'osi', 'osi_mean',
+                  'gts1', 'gts2', 'gts3', 'gts4', 'gts5', 'gts6']
   
   df['year'] = int(year)
   df['semester'] = int(semester)
   df['level'] = level
+  df['program_code'] = program_code
   
-  # create alternate course_codes
-  temp = df['course_code_ces'].str.split('-', n=1, expand=True)
-  df['course_code'] = temp[0]
-  df['course_code_ces2'] = temp[0] + '-' + temp[1].str[:2]
-  df['course_code_ces2'] = df['course_code_ces2'].combine_first(df['course_code'])
-  df['cluster_code'] = temp[1]
+  # prepare data for program_course_level table
+  df_courses = df.loc[df['all_flag'] != 'All']
+  df_courses = df_courses.loc[df['all_flag'].notnull()]
   
-  
-  # prepare data for course_level table
-  df_courses = df.loc[df['all_flag'] == 'All']
-
   df_courses: object = df_courses[[
-    'year', 'semester', 'level',
-    'course_code', 'course_code_ces', 'course_code_ces2',
-    'course_name',
-    'course_coordinator', 'career', 'campus',
-    'international_count', 'domestic_count', 'ft_count', 'pt_count',
-    'population', 'reliability',
-    'osi_count', 'osi', 'osi_mean',
-    'gts_count', 'gts', 'gts_mean',
+    'year', 'semester', 'level', 'program_code',
+    'course_code_ces', 'class_nbr',
+    'population', 'osi_count', 'gts_count', 'reliability',
+    'gts', 'gts_mean', 'osi', 'osi_mean',
     'gts1', 'gts2', 'gts3', 'gts4', 'gts5', 'gts6'
   ]]
 
@@ -81,68 +86,66 @@ def upload_course_data_from_excel(directory, filename, engine,
     = df_courses[['gts', 'gts_mean', 'osi', 'osi_mean',
                   'gts1', 'gts2', 'gts3', 'gts4', 'gts5', 'gts6']].apply(pd.to_numeric, errors='coerce')
 
+  df_courses.class_nbr = df_courses.class_nbr.astype(np.int64)
+
+  df_courses['class_nbr'].apply(str)
+  
   try:
     df_courses.to_sql(
-      name=course_tbl_name,
+      name=program_course_tbl_name,
       con=engine,
       schema=schema,
       if_exists='append',
       index=False
       )
   except Exception as e:
+    print(e)
     print('course input failed' + filename)
     pass
   
   # prepare data for teacher_course_level table
-  df_teacher = df.loc[df['class_nbr'].notnull()]
+  df_program = df.loc[df['osi_count'].notnull()]
+  df_program = df.loc[(df['course_code_ces'].str.contains(program_code)) & df['class_nbr'].isnull()]
 
-  df_teacher: object = df_teacher[[
+
+  df_program: object = df_program[[
     'year', 'semester', 'level',
-    'course_code', 'course_code_ces', 'course_code_ces2',
-    'class_nbr', 'term_code', 'section_code',
-    'course_name',
-    'teaching_staff',
-    'course_coordinator', 'career', 'campus',
-    'gts_count', 'gts', 'gts_mean',
+    'program_code',
+    'population', 'osi_count', 'gts_count', 'reliability',
+    'gts', 'gts_mean', 'osi', 'osi_mean',
     'gts1', 'gts2', 'gts3', 'gts4', 'gts5', 'gts6'
   ]]
 
-  df_teacher = df_teacher.infer_objects()
+  df_program = df_program.infer_objects()
 
-  df_teacher[['gts', 'gts_mean',
+  df_program[['gts', 'gts_mean',
               'gts1', 'gts2', 'gts3', 'gts4', 'gts5', 'gts6']] \
-    = df_teacher[['gts', 'gts_mean',
+    = df_program[['gts', 'gts_mean',
                   'gts1', 'gts2', 'gts3', 'gts4', 'gts5', 'gts6']].apply(pd.to_numeric, errors='coerce')
 
-  df_teacher.term_code = df_teacher.term_code.astype(np.int64)
-  df_teacher.class_nbr = df_teacher.class_nbr.astype(np.int64)
   
-  df_teacher['class_nbr'].apply(str)
-  df_teacher['term_code'].apply(str)
   
   try:
-    df_teacher.to_sql(
-      name=class_teacher_tbl_name,
+    df_program.to_sql(
+      name=program_tbl_name,
       con=engine,
       schema=schema,
       if_exists='append',
       index=False
       )
   except Exception as e:
+    print(e)
     print('teacher input failed' + filename)
     pass
 
-
-  
-  
 # get data from excel doc
 # open template
-directory = 'H:\\Data\\CoB Database\\CES\\\class_teacher\\'
+directory = 'H:\\Data\\CoB Database\\CES\\Program_course\\Done\\'
 
 for filename in os.listdir(directory):
     if filename.endswith(".xls"):
         print(os.path.join(directory, filename))
-        upload_course_data_from_excel(directory, filename, postgres_engine)
+        upload_program_course_data_from_excel(directory, filename, postgres_engine)
         continue
     else:
         continue
