17e31d101e0070cd7b9d5154f8bba11c5e9443c5
pjryan356 <peter.ryan2@rmit.edu.au>
2019-01-07 17:30:24 +1100

    Update PLO_CLO_mapping.py

--- Other Reports/PLO_CLO_mapping.py
+++ Other Reports/PLO_CLO_mapping.py
@@ -4,34 +4,11 @@
 import pandas as pd
 import openpyxl
 from openpyxl.styles import Alignment
-import datetime as dt
 import shutil
 from tabulate import tabulate
 
 import sys
 sys.path.append('c:\\Peter\\GitHub\\CoB\\')
-
-program_code = 'BP251'
-
-def get_school_name(school_code):
-  if school_code == '610P':
-    return 'CPO'
-  if school_code == '615H':
-    return 'ACCT'
-  if school_code == '620H':
-    return 'BITL'
-  if school_code == '625H':
-    return 'EFM'
-  if school_code == '630H':
-    return 'MGT'
-  if school_code == '650T':
-    return 'VBE'
-  if school_code == '660H':
-    return 'GSBL'
-  if school_code == 'VN':
-    return 'SBM'
-  return None
-
 
 
 def copy_rename(old_file_name, new_file_name):
@@ -46,77 +23,25 @@ def copy_rename(old_file_name, new_file_name):
   new_dst_file_name = os.path.join(dst_dir, new_file_name)
   os.rename(dst_file, new_dst_file_name)
 
-def get_CLOs(text):
-  CLOs = text.split('<li>')
-  CLO_list = []
-  for clo in CLOs[1:]:
-    clo = clo.replace('<br />', '\n')
-    clo = clo.split('</li>')[0]
-    CLO_list.append(clo)
-  return CLO_list
-
-# open template
-directory = 'H:\\Projects\\CoB\\Program Transformation\\2019\\'
-clo_filename = 'CLOs_{}.xlsx'.format(program_code)
-plo_filename = 'PLOs_{}.xlsx'.format(program_code)
-mapping_filename = 'Active_programs_core_course_list_2018.xlsx'
-template = 'PLO_CLO_alignment_template.xlsx'
-savefile = 'PLO_CLO_alignment_template_{}.xlsx'.format(program_code)
 
 
 # open template
-wb = openpyxl.load_workbook(directory+template)
+directory = 'H:\\Projects\\CoB\\Program Transformation\\CLO mapping\\'
+plo_clo_filename = 'PLOs_CLOs_manually_editted.xlsx'
 
+template = 'PLO_CLO_alignment_template_1.xlsx'
 
-# fill CLOs worksheet
-clo_df = pd.read_excel(open(directory+filename, 'rb'))
-clo_ws = wb["CLOs"]
-
-j = 2
-for i, r in clo_df.iterrows():
-  CLO_list = get_CLOs(r[6])
-  k = 1
-  for clo in CLO_list:
-    clo_ws.cell(row=j, column=1).value = r[0]
-    clo_ws.cell(row=j, column=2).value = r[1]
-    clo_ws.cell(row=j, column=3).value = r[3]
-    clo_ws.cell(row=j, column=4).value = get_school_name(r[3])
-    clo_ws.cell(row=j, column=5).value = 'CLO{}'.format(k)
-
-    clo_ws.cell(row=j, column=6).alignment = Alignment(wrapText=True)
-    clo_ws.cell(row=j, column=6).value = clo
-    j += 1
-    k += 1
-wb.save(directory+savefile)
-
-# fill PLOs worksheet
-plo_df = pd.read_excel(open(directory+filename, 'rb'))
-plo_ws = wb["PLOs"]
-
-j = 2
-for i, r in plo_df.iterrows():
-  PLO_list = get_PLOs(r[6])
-  k = 1
-  for plo in PLO_list:
-    plo_ws.cell(row=j, column=1).value = r[0]
-    plo_ws.cell(row=j, column=2).value = r[1]
-    plo_ws.cell(row=j, column=3).value = r[3]
-    plo_ws.cell(row=j, column=4).value = get_school_name(r[3])
-    plo_ws.cell(row=j, column=5).value = 'CLO{}'.format(k)
-
-    plo_ws.cell(row=j, column=6).alignment = Alignment(wrapText=True)
-    plo_ws.cell(row=j, column=6).value = plo
-    j += 1
-    k += 1
-
-wb.save(directory+savefile)
-
-# Fill Course Mapping worksheet
-mapping_df = pd.read_excel(open(directory+mapping_filename, 'rb'))
-clo_ws = wb["Mapping"]
+# open template
+#wb = openpyxl.load_workbook(directory+template)
 
 
 
-wb.save(directory+savefile)
+# Get Dataframes
+df_plo = pd.read_excel(directory+plo_clo_filename, sheetname='PLOs')
+df_clo = pd.read_excel(directory+plo_clo_filename, sheetname='CLOs')
+df_mapping = pd.read_excel(directory+plo_clo_filename, sheetname='Mapping')
+
+print(tabulate(df_mapping, headers='keys'))
+
 
 
