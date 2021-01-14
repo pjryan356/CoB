15b0e08b5d8c5a0078355b9656380b257da5f4be
pjryan356 <peter.ryan2@rmit.edu.au>
2021-01-15 09:29:46 +1100

    Closing off
    I will be using the LA repository from now on

--- course_pack/file_distribution.py
+++ course_pack/file_distribution.py
@@ -22,19 +22,20 @@ def create_directory(dirName):
 semester = '2020S1'
 semester = input("Input Semester [e.g. 2019S1]: ") ## Input password
 
-folder = 'H:\\Projects\\CoB\\CES\\Course Enhancement\\2020 S1\\DatPacks\\{}\\'.format(semester)
+folder = 'H:\\Projects\\CoB\\Surveys\\CES\\Course Enhancement\\2020 S1\\DataPacks\\'
 
 for sch in ['ACCT', 'BITL', 'EFM', 'GSBL', 'MGT', 'VBE']:
   
   sch_folder = folder + '{}\\'.format(sch)
-
+  
+  print(sch_folder)
   for filename in os.listdir(sch_folder):
     if filename.endswith(".pdf"):
       course_code = filename.split(' ')[0]
       crse_folder = sch_folder+'{}\\'.format(course_code)
-      create_directory(crse_folder)
+      #create_directory(crse_folder)
 
-      shutil.copy(sch_folder+filename, crse_folder+filename)
+      #shutil.copy(sch_folder+filename, crse_folder+filename)
       print(crse_folder+filename)
       continue
     else:
